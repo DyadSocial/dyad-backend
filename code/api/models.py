84174ca73f013fd6647b40c96f9cbc14bc77d13a
Jake D <jakob.delossantos1@gmail.com>
2021-11-29 18:24:00 -0800

    feat(class): editing models and settings

--- code/api/models.py
+++ code/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    user = models.CharField(max_length=24)
+    userid = models.IntegerField(default=0)
+    date = models.DateTimeField('Date Created')
+    optional_image = 
+
--- code/app/settings.py
+++ code/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'core',
+    'api'
 ]
 
 MIDDLEWARE = [
