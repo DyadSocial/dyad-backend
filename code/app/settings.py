86797a9eb490cbb2692192480f30f50dcd7ee3fa
Vincent Pham <vpham@nevada.unr.edu>
2022-04-25 01:47:27 -0700

    uwsgi settings

--- code/app/settings.py
+++ code/app/settings.py
@@ -30,8 +30,8 @@ DEBUG = True
 
 
 if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'widget_tweaks',
     'core',
-    'images',
+    #'images',
     'api',
     'rest_framework',
     # 'corsheaders',
--- code/app/urls.py
+++ code/app/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include("core.urls")),
     path('chat/', include("chat.urls")),
-    path('images/', include("images.urls"))
+#    path('images/', include("images.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
